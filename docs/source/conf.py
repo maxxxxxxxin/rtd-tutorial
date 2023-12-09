28f3c95c1879467a83e316e66e9c11a2a7439391
maxxxxxxxin <108003917+maxxxxxxxin@users.noreply.github.com>
2023-12-09 16:30:32 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,12 +12,14 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'nbsphinx',
+    'sphinx.ext.autodoc',  # Core Sphinx library for auto html doc generation from docstrings
+    'sphinx.ext.autosummary',  # Create neat summary tables for modules/classes/methods etc
+    'sphinx.ext.intersphinx',  # Link to other project's documentation (see mapping below)
+    'sphinx.ext.viewcode',  # Add a link to the Python source code for classes, functions etc.
+    'sphinx_autodoc_typehints', # Automatically document param types (less noise in class signature)
+    'nbsphinx',  # Integrate Jupyter Notebooks and Sphinx
+    'IPython.sphinxext.ipython_console_highlighting',
+    'sphinx.ext.napoleon'
 ]
 
 intersphinx_mapping = {
